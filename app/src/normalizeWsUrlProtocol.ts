f8b09e7eae5a42465ef19731f33e3d32c1332341
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2023-03-08 05:10:00 +0100

    dump

--- app/src/normalizeWsUrlProtocol.ts
+++ app/src/normalizeWsUrlProtocol.ts
@@ -1,7 +1,20 @@
-dependencyImports
-
-export function normalizeWsUrlProtocol() {
+export function normalizeWsUrlProtocol(url: string) {
   
+  if (url.startsWith("ws://")) return url
+  else if (url.startsWith("wss://")) return url
+  else if (url.startsWith("http://")) return "ws://" + url.slice(7)
+  else if (url.startsWith("https://")) return "wss://" + url.slice(8)
+  else {
+    if (!url.startsWith("/")) url = "/" + url
+    try {
+      return normalizeWsUrlProtocol(`${location.origin}${url}`)
+    }
+    catch(e) {
+      throw new Error("Please provide a valid fully qualified url (starting with wss://). Got: " + url)
+    }
+    
+  }
 }
+
 
 export default normalizeWsUrlProtocol
